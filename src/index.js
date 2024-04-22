b7fd50d942ac856d6fc7649f0f6160104eca5dcf
edwinporras <edwinporras2012@gmail.com>
2024-04-22 18:49:47 -0500

    definiendo rutas del controlador

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //Cambiarlo despues or un modulo
 const express = require('express');
-const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/api/v1', v1Router);
+app.use('/api/v1/workouts', v1WorkoutRouter);
 
 app.listen(PORT, () => {console.log(`Servidor en el puerto ${PORT}`)})
